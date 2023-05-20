47d5cd144590f561c3c7685d99231f017b304575
Hasib <hasibhosen7612@gmail.com>
2023-05-20 19:30:09 +0600

    Assignment 03

---
+++ Module 03/multiplicationTable.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+  }
+}
+
+// multiplicationTable(10);
+// multiplicationTable(5);
+multiplicationTable(2);
